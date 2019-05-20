64a5bf58f72d9333280f949695ac6cf530231f47
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-05-20 19:59:07 +0200

    Remove dummy file

--- src/main/kotlin/io/github/spacialcircumstances/jarpacker/MainController.kt
+++ src/main/kotlin/io/github/spacialcircumstances/jarpacker/MainController.kt
@@ -1,11 +1,12 @@
 package io.github.spacialcircumstances.jarpacker
 
 import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import tornadofx.Controller
 import java.io.File
 
 class MainController: Controller() {
-    val files = FXCollections.observableArrayList<String>("Test.txt")
+    val files: ObservableList<String> = FXCollections.observableArrayList()
 
     fun addFiles(file: List<File>) {
         file.forEach {
