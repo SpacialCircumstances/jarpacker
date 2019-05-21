ea11b43b7ff3ccbfa187a98cb4371cda3b6e3ad8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-05-21 14:55:27 +0200

    Check if files for packaging exist and delete old jar file

--- src/main/kotlin/io/github/spacialcircumstances/jarpacker/JarBuilder.kt
+++ src/main/kotlin/io/github/spacialcircumstances/jarpacker/JarBuilder.kt
@@ -4,5 +4,17 @@ import java.io.File
 
 fun buildJar(files: List<File>, outFile: File): String {
     val sb = StringBuilder()
+    if (outFile.exists()) {
+        outFile.delete()
+        sb.appendln("File ${outFile.path} deleted.")
+    }
+    files.forEach {
+        if (it.exists()) {
+            sb.appendln("File ${it.path} found. Great!")
+        } else {
+            sb.appendln("File ${it.path} not found. Aborting.")
+            return sb.toString()
+        }
+    }
     return sb.toString()
 }
